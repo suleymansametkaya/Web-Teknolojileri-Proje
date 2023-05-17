1c68871fbdb5b93d8c1a4adff84e2c3728b72c64
Süleyman Samet Kaya <69515911+suleymansametkaya@users.noreply.github.com>
2023-05-17 21:12:28 +0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,1 +1,17 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    var apodTitle = document.getElementById("api-baslik");
+    var apodImage = document.getElementById("api-resim");
+    var apodExplanation = document.getElementById("api-aciklama");
+  
+    fetch("https://api.nasa.gov/planetary/apod?api_key=yV9YYOOxLNrbp6x68brNDcaHxcxdvFK21HOMRuuU")
+      .then(response => response.json())
+      .then(data => {
+        apodTitle.textContent = data.title;
+        apodImage.src = data.url;
+        apodExplanation.textContent = data.explanation;
+      })
+      .catch(error => {
+        console.log("Hata: " + error);
+      });
+  });
+  
