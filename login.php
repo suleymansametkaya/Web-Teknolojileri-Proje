c2e2852039cdcbce2509581bcdfa7ee2e6cc6ef4
Süleyman Samet Kaya <69515911+suleymansametkaya@users.noreply.github.com>
2023-05-21 23:06:56 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giriş Sayfası</title>
     <link rel="stylesheet" type="text/css" href="css/loginphp.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
